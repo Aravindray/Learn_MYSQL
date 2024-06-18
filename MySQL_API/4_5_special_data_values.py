e6beebf9a29f97634c06fb938554c823ae681f13
Aravindray <aravindselvasan@gmail.com>
2024-06-18 15:32:55 +0530

    modified some

--- MySQL_API/4_5_special_data_values.py
+++ MySQL_API/4_5_special_data_values.py
@@ -1,5 +1,4 @@
 '''Handling special characters and NULL values is important and it also prevent sql injection attack'''
-import mysql.connector
 import cookbook
 
 # Connecting to server
@@ -7,7 +6,8 @@ conn = cookbook.connect()
 cursor = conn.cursor()
 
 # Using Placeholder
-cursor.execute("INSERT INTO profile (name, birth, color, foods, cats) VALUES (%s, %s, %s, %s, %s)", ("De'mont", '1973-01-12', None, "eggroll", 4))
+cursor.execute("INSERT INTO profile (name, birth, color, foods, cats) VALUES (%s,\
+                %s, %s, %s, %s)", ("De'mont", '1973-01-12', None, "eggroll", 4))
 
 cursor.close()
 conn.commit()
@@ -29,3 +29,5 @@ for (id, name, cats) in cursor:
     print(f'id: {id}, name: {name}, cats: {cats}')
 print(f'No of rows returned {cursor.rowcount}')
 cursor.close()
+
+conn.close()
