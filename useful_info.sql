d6fc9a6f1c7246a22e4017f2f5ed89a554b1a4e8
Aravindray <aravindselvasan@gmail.com>
2024-06-26 11:58:25 +0530

    learned table copying methods

--- useful_info.sql
+++ useful_info.sql
@@ -1,4 +1,11 @@
+-- To list all the table name based on creation date
+
 SELECT TABLE_SCHEMA, TABLE_NAME, create_time
 FROM information_schema.TABLES
-WHERE TABLE_SCHEMA = 'cookbook'
+WHERE TABLE_SCHEMA = 'cookbook' -- change the database name
 ORDER BY create_time DESC;
+
+-- To check the tablespace of the table
+
+SELECT NAME, SPACE_TYPE FROM INFORMATION_SCHEMA.INNODB_TABLESPACES
+WHERE NAME LIKE 'cookbook/%'; -- add the table name with database
